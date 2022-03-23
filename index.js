5088475431879c5832445a5de780c36f965644ef
Courtney Carroll <bcakescw@gmail.com>
2022-03-23 19:24:08 -0400

    need to use variable i

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
-weeklyExpenses = weeklyExpenses + answer
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
