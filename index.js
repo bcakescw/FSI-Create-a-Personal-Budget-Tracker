d7771e8945a56414dbd0a41a9e3d9a331440d69f
Courtney Carroll <bcakescw@gmail.com>
2022-03-23 19:10:35 -0400

    Collecting the User's Answer 2

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-let answer = window.prompt (weeklyExpenseQuestions[0])
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
 console.log(answer)
